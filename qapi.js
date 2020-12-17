499f9d4dece63f8176553ecfed98004b04e777d8
asaini2006 <76147667+asaini2006@users.noreply.github.com>
2020-12-17 11:52:01 +0530

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fdaf5ccd84ef0001e94c8f1",
+				"5fdaf5ccd84ef0001e94c8f3"
+			]
+		},
+		"5fdaf5ccd84ef0001e94c8f1": {
+			"id": "5fdaf5ccd84ef0001e94c8f1",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fdaf5ccd84ef0001e94c8f3": {
+			"id": "5fdaf5ccd84ef0001e94c8f3",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
